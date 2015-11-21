bce4e4dbc51434dfdb5ed8daba780ced5888a0da
achatterjee7 <abhishek.rob@gmail.com>
2015-11-21 02:51:31 -0500

    Fixing duplicate fields.

--- src/jvm/movingAverageWithSpikeDetection/SpikeDetectionBolt.java
+++ src/jvm/movingAverageWithSpikeDetection/SpikeDetectionBolt.java
@@ -45,7 +45,7 @@ public class SpikeDetectionBolt implements IBasicBolt {
 	
 	@Override
 	public void declareOutputFields(final OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("string", "double", "double", "string"));
+		declarer.declare(new Fields("string1", "double1", "double2", "string2"));
 	}
 
 	@Override
